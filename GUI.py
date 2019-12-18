8a48aa00f678a0b23b706a3a8074c77dfa0060b7
Bobbie van Gorp <bobbievangorp@live.nl>
2019-12-18 23:34:33 +0100

    Add injuries to GUI

--- GUI.py
+++ GUI.py
@@ -1,4 +1,4 @@
-# import PySimpleGUI as sg
+import PySimpleGUI as sg
 import random
 
 # The lists of exercises for all muscle groups.
@@ -163,55 +163,98 @@ def parse_input(data, preferences, injuries):
     profile = [data, injuries, preferences]
     return profile
 
+def get_data():
+    data = None
+    preferences = None
+    injuries = None
+    
+    sg.change_look_and_feel('Reddit')    # Add a touch of color
+    # All the stuff inside your window.
+    layout = [  
+        [sg.Text('Please fill in client information and click Next')],
+        [sg.Text('Age'), sg.InputText()],
+        [sg.Text('Gender'), sg.InputCombo(('Male', 'Female'), size=(20, 3))],
+        [sg.Text('Weight in kg'), sg.InputText()],
+        [sg.Text('Time available per week in hours'), sg.InputText()],
+        [sg.Text('Training level (1-5)'), sg.Slider(range=(1, 5), orientation='h', size=(10, 20), default_value=1)],
+        [sg.Checkbox('Client has followed training schedules before')],
+        
+        [sg.Text('Goal'), sg.InputCombo(('Rehabilitation', 'General', 'Weight loss', 'Physical therapy', 'Powerlifting', 
+                                          'General strength', 'Bodybuilding', 'General aesthetics', 'General endurance', 
+                                          'Specific endurance'), size=(20, 10))],
+        [sg.Checkbox('Client has specific preferences')],
+        [sg.Checkbox('Client has one or more injuries')],
+        
+        [sg.Button('Next'), sg.Button('Cancel')] ]
+    
+    # Create the Window
+    window = sg.Window('Create schedule', layout)
+    
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event in (None, 'Cancel'):   # if user closes window or clicks cancel
+            break
+        data = values 
+        
+        window.close()
+        
+    if data[7] == True:
+        layout = [  
+        [sg.Text('Please fill in preferences and click Next')],
+        
+        [sg.Button('Next'), sg.Button('Cancel')] ]
+        
+        window = sg.Window('Preferences', layout)
+        
+        while True:
+            event, values = window.read()
+            if event in (None, 'Cancel'):   # if user closes window or clicks cancel
+                break
+            preferences = values 
+            
+            window.close()
+    
+    if data[8] == True:
+        layout = [  
+        [sg.Text('Please check where the client has an injury and click Next')],
+        [sg.Text('Injury 1'), sg.InputCombo(('Chest', 'Back', 'Legs', 'Biceps', 'Triceps', 
+                                          'Abs', 'Shoulders'), default_value='Chest', size=(20, 7))],
+        [sg.Text('Injury 2'), sg.InputCombo(('None', 'Chest', 'Back', 'Legs', 'Biceps', 'Triceps', 
+                                          'Abs', 'Shoulders'), default_value='None', size=(20, 7))],
+        [sg.Text('Injury 3'), sg.InputCombo(('None', 'Chest', 'Back', 'Legs', 'Biceps', 'Triceps', 
+                                          'Abs', 'Shoulders'), default_value='None', size=(20, 7))],
+        
+        [sg.Button('Next'), sg.Button('Cancel')] ]
+        
+        window = sg.Window('Injuries', layout)
+        
+        while True:
+            event, values = window.read()
+            if event in (None, 'Cancel'):   # if user closes window or clicks cancel
+                break
+            values = list(values.values())
+            injuries = [x for x in values if x != 'None']
+        
+            window.close()
+            
+    data = list(data.values())[:7]
+    
+    return data, preferences, injuries
+
 def main():
-    # sg.change_look_and_feel('Reddit')    # Add a touch of color
-
-    # # All the stuff inside your window.
-    # layout = [  
-    #     [sg.Text('Please fill in client information and click Ok')],
-    #     [sg.Text('Age'), sg.InputText()],
-    #     [sg.Text('Gender'), sg.InputCombo(('Male', 'Female'), size=(20, 3))],
-    #     [sg.Text('Weight in kg'), sg.InputText()],
-    #     [sg.Text('Time available per week in hours'), sg.InputText()],
-    #     [sg.Text('Training level (1-5)'), sg.InputText()],
-    #     [sg.Checkbox('Client has followed training schedules before')],
-        
-    #     [sg.Text('Goal'), sg.InputCombo(('Rehabilitation', 'General', 'Weight loss', 'Physical therapy', 'Powerlifting', 
-    #                                       'General strength', 'Bodybuilding', 'General aesthetics', 'General endurance', 
-    #                                       'Specific endurance'), size=(20, 10))],
-    #     [sg.Checkbox('Client has specific preferences')],
-    #     [sg.Checkbox('Client has one or more injuries')],
-        
-    #     [sg.Button('Next'), sg.Button('Cancel')] ]
-
-    # # Create the Window
-    # window = sg.Window('Window Title', layout)
-
+    
+    data, preferences, injuries = get_data()
     # TEST INPUTS BELOW, this is how the data should look once received from the input:
 
     # Data format: [Age, Gender, Weight, Hours, Level, TrainingScheduleFollowed, Goal]
-    data = [18, "M", 70, 3, 5, False, "Strength"]
+    #data = [18, "M", 70, 3, 5, False, "Strength"]
 
     # List of preferences. Always, in preferences, define exercises to remove from schedule.
     preferences = []
 
     # List of injuries.
-    injuries = []
-
-    # # Event Loop to process "events" and get the "values" of the inputs.
-    # while True:
-    #     event, values = window.read()
-    #     if event in (None, 'Cancel'):   # if user closes window or clicks cancel.
-    #         break
-    #     print(values)
-    #     data = values 
-        
-    #     #if data[7] == True:
-        
-    #     #if data[8] == True:
-            
-
-    # window.close()
+    #injuries = []
 
     # Parse all input. All information will be stored inside the profile.
     profile = parse_input(data, preferences, injuries)
